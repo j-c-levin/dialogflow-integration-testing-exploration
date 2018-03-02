650108b8fb523317b4ee3fe28ddb698a7de11fa7
Joshua Levin <joshua.levin@holition.com>
2018-03-02 14:04:38 +0000

    remove unnecessary function from e2e testing

--- source/e2e/index.e2e.ts
+++ source/e2e/index.e2e.ts
@@ -5,27 +5,23 @@ import { Attempt } from './conversation/begin_game/attempt';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-async function test() {
-    describe('critical path: playing the game', function () {
-        let conversation: Conversation, attempt: Attempt;
-        beforeEach(async () => {
-            conversation = new Conversation();
-            const game = await conversation.start();
-            attempt = await game.begin(conversation);
-        });
-        it('finds the right answer', async function () {
-            const rightAnswer = await attempt.getAnswer();
-            expect(rightAnswer).to.not.be.undefined;
-        });
-        it('says higher', async function () {
-            const wrongAnswer = await attempt.makeGuess(-1);
-            expect(wrongAnswer.result.fulfillment.speech).to.equal('Higher!');
-        });
-        it('says lower', async function () {
-            const wrongAnswer = await attempt.makeGuess(6);
-            expect(wrongAnswer.result.fulfillment.speech).to.equal('Lower!');
-        });
+describe('critical path: playing the game', function () {
+    let conversation: Conversation, attempt: Attempt;
+    beforeEach(async () => {
+        conversation = new Conversation();
+        const game = await conversation.start();
+        attempt = await game.begin(conversation);
     });
-}
-
-test();
+    it('finds the right answer', async function () {
+        const rightAnswer = await attempt.getAnswer();
+        expect(rightAnswer).to.not.be.undefined;
+    });
+    it('says higher', async function () {
+        const wrongAnswer = await attempt.makeGuess(-1);
+        expect(wrongAnswer.result.fulfillment.speech).to.equal('Higher!');
+    });
+    it('says lower', async function () {
+        const wrongAnswer = await attempt.makeGuess(6);
+        expect(wrongAnswer.result.fulfillment.speech).to.equal('Lower!');
+    });
+});
